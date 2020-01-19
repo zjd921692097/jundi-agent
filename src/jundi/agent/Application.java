ef35d3de45e62db56b06174469a315cc490f94a8
jundizhou <jundizhou@harmonycloud.com>
2020-01-19 16:02:20 +0800

    Initial commit

---
+++ src/jundi/agent/Application.java
@@ -0,0 +1,12 @@
+package jundi.agent;
+
+/**
+ * @author ：jundi zhou
+ * @date ：Created in 2020-01-19 15:57
+ * @description：start class
+ * @modified By：
+ * @version: $
+ */
+public class Application {
+
+}
